ce21bb9d18efe3ab26faabed85cab21ad8d83587
Abdullah Arslan <121155456+Vi314@users.noreply.github.com>
2023-10-19 16:47:30 +0300

    .

---
+++ ConsoleApp1/Data Structures/BinaryTreeDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Demonstrations.Data_Structures {
+    //? Brain not suitable for coding, to be continued
+    public class BinaryTree {
+        //public BinaryTree(Node root) {
+        //    Root = root;
+        //    Nodes.Add(root);
+        //}
+        //public BinaryTree() {
+        //    Root = new Node(1);
+        //    Nodes.Add(Root);
+        //}
+        //private List<Node> Nodes = new List<Node>();
+        //public Node Root { get; set; }
+        //public class Node(int keyVal = 0, Node? leftNode = null, Node? rightNode = null) {
+        //    internal int key = keyVal;
+        //    internal Node? left = leftNode;
+        //    internal Node? right = rightNode;
+        //}
+        //public int AddNode(int parentKey, Node node) {
+        //    Node? parent = Nodes.Where(x => x.key == parentKey).First();
+        //    if(parent is null) {
+        //        return 1;
+        //    }
+        //    return 0;
+        //}
+
+    }
+    public class BinaryTreeDemo : IDemonstration {
+        public void Run() {
+            throw new NotImplementedException();
+        }
+    }
+}
